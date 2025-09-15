a213739fcb431eeafb08c7da31d2eb9cb31b3662
David Akinola <dakinola54@gmail.com>
2025-09-15 12:57:11 +0100

    modified schema build for flashcards

--- models/Flashcard.js
+++ models/Flashcard.js
@@ -3,7 +3,7 @@
 const mongoose = require("mongoose");
 
 const flashcardSchema = mongoose.Schema({
-    question: {type: String, required: true},
+    question: {type: String, required: true, unique: true},
     answer: {type: String, required: true},
 })
 
